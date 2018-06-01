d4d2e85c87471134f0c079b4c0dd238835629014
pedrogallon <pedrogallonalves@hotmail.com>
2018-06-01 00:58:57 -0300

    Finalizado

---
+++ src/Exercicio8_1/Teste.java
@@ -0,0 +1,14 @@
+package Exercicio8_1;
+
+public class Teste {
+	public static void main(String[] args) {
+		
+	NoticiarioAssina noticiario = new NoticiarioAssina();
+	new Consumidor("Pedro Gallon", noticiario);
+	noticiario.notificaNoticia("Uma nova noticia", 1, 2, "Tópicos interessantes!");
+	new Consumidor("Jão", noticiario);
+	noticiario.notificaNoticia("Uma noticia mais nova ainda", 2, 2, "Tópico ruim");
+	
+	
+	}
+}
---
+++ src/Exercicio9_1/Slot.java
@@ -0,0 +1,6 @@
+package Exercicio9_1;
+
+public interface Slot {
+	public double recebeMoeda(int m);
+	public void setSlot(Slot s);
+}
